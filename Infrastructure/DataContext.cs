dbda6c34df169a5bb1d301c9d072b2e107994bb2
mohamedsamy871 <mohamedsamy871@gmail.com>
2022-08-01 11:20:46 +0200

    Add project files.

---
+++ Infrastructure/DataContext.cs
@@ -0,0 +1,25 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace Infrastructure
+{
+    public class DataContext : IdentityDbContext
+    {
+        public DataContext()
+        {
+
+        }
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<LoginItem> LoginItems { get; set; }
+    }
+}
